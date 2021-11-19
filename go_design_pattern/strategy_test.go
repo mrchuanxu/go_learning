5ca84b26fe7cdbc5430d44ff6077e2eb3db8a495
zhangcx01 <zhangcx01@mingyuanyun.com>
2021-11-19 12:58:23 +0800

    strategy

--- go_design_pattern/strategy_test.go
+++ go_design_pattern/strategy_test.go
@@ -1,4 +1,25 @@
 package go_design_pattern_test
 
 // 定义一族算法，将每个算法分别封装起来，让他们可以互相替换。
+type FuncSort func(string)string
 
+func QuickSort(string)string{
+	return "quickSort"
+}
+
+func ExternalSort(string)string{
+	return "ExternalSort"
+}
+
+func ConcurrentSort(string)string{
+	return "ConcurrentSort"
+}
+
+var funcSortsM = map[string]FuncSort{
+	"quickSort":QuickSort,"ExternalSort":ExternalSort,"ConcurrentSort":ConcurrentSort,
+}
+
+func sortFile(f string)string{
+	sortFunc := funcSortsM["quickSort"](f)
+	return sortFunc
+}
